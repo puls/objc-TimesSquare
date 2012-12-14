5e56feadd8d1346f14120da24433f99c3475bcaf
Michael White <mwhite@squareup.com>
2012-12-14 01:57:23 -0800

    Project changes to make TimesSquare easier to use as a submodule

--- TimesSquare/TimesSquare.h
+++ TimesSquare/TimesSquare.h
@@ -7,6 +7,6 @@
 //  See the LICENSE file distributed with this work for the terms under
 //  which Square, Inc. licenses this file to you.
 
-#import "TSQCalendarMonthHeaderCell.h"
-#import "TSQCalendarRowCell.h"
-#import "TSQCalendarView.h"
+#import <TimesSquare/TSQCalendarMonthHeaderCell.h>
+#import <TimesSquare/TSQCalendarRowCell.h>
+#import <TimesSquare/TSQCalendarView.h>
