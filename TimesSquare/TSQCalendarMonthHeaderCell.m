05fa303d1b286e2e7f6f07ff6ce73a375d890547
Ryan Holmes <rholmes@opentable.com>
2013-08-24 21:05:11 -0700

    Replace deprecated UITextAlignmentCenter with NSTextAlignmentCenter

--- TimesSquare/TSQCalendarMonthHeaderCell.m
+++ TimesSquare/TSQCalendarMonthHeaderCell.m
@@ -70,7 +70,7 @@ static const CGFloat TSQCalendarMonthHeaderCellMonthsHeight = 20.f;
     for (NSUInteger index = 0; index < self.daysInWeek; index++) {
         NSInteger ordinality = [self.calendar ordinalityOfUnit:NSDayCalendarUnit inUnit:NSWeekCalendarUnit forDate:referenceDate];
         UILabel *label = [[UILabel alloc] initWithFrame:self.frame];
-        label.textAlignment = UITextAlignmentCenter;
+        label.textAlignment = NSTextAlignmentCenter;
         label.text = [dayFormatter stringFromDate:referenceDate];
         label.font = [UIFont boldSystemFontOfSize:12.f];
         label.backgroundColor = self.backgroundColor;
@@ -85,7 +85,7 @@ static const CGFloat TSQCalendarMonthHeaderCellMonthsHeight = 20.f;
     }
     
     self.headerLabels = headerLabels;
-    self.textLabel.textAlignment = UITextAlignmentCenter;
+    self.textLabel.textAlignment = NSTextAlignmentCenter;
     self.textLabel.textColor = self.textColor;
     self.textLabel.shadowColor = [UIColor whiteColor];
     self.textLabel.shadowOffset = self.shadowOffset;
